d01518a616bdcbd8aaa48c3e0992c939e0a8ab3f
Jaylin Bratcher-McEachern <651608@aacps.org>
2020-09-22 21:09:07 +0000

    this is my creature slime

--- index.java
+++ index.java
@@ -11,13 +11,28 @@ void draw() {
  
   //Make a creature!!! 
   //all of your programming should be after this 👍
-  
- 
-  
-  
- 
+ //slime's body
+  rect(200, 200, 140, 150)
+ fill(50,205,50)
+ //slimes's 1st leg
+  rect(245, 310, 20, 70)
+  //slime's 2nd leg
+  rect(155, 310, 20, 70)
+  //slime's 1st arm 
+  rect(92, 185, 75, 20)
+ //slims's 2nd arm
+ rect(307, 185, 75, 20)
+ //slimes 1st eye
+ellipse(170, 150, 20, 20)
+//slimes 2nd eye
+ellipse(225, 150, 20, 20)
+//slimes mouth 
+ellipse(200, 215, 50, 50)
+//slimes 1st foot
+ellipse(250, 350, 50, 10)
+//slimes 2nd foot 
+ellipse(150, 350, 50, 10) 
 
-  
  
 	
 
